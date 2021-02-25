3f757d0c566b1fb0d27ae7b2cc0ba53a8ebd4e71
superymk <you@example.com>
2021-02-24 20:31:33 -0500

    Upload final proof code

--- WK_Design/v.bat
+++
@@ -1,5 +0,0 @@
-dafny /trace /stats /errorTrace:2 /compile:0 /timeLimit:60 test3.dfy
-
-@rem Syntax.dfy Model.dfy Utils.dfy Util_Lemmas.dfy ProtectionConstraints.dfy Mappings_Subjs.dfy Mappings_Objs.dfy Properties.dfy  ./utils/Collections.dfy
-@rem Method1.dfy
-
